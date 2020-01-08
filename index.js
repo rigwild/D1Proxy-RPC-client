fc559db2d979ae588758a694c6522bf35768b768
rigwild <contact@asauvage.fr>
2020-01-08 11:20:21 +0100

    Added RPC messages

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ ws.on('message', data => {
   }
   else if (data.startsWith('sniffer_sent ')) {
     const packet = data.substring(13)
-    console.log(`[SNIFFER][SENT____] ${packet}`)
+    console.log(`[SNIFFER][__SENT__] ${packet}`)
+  }
+  else if (data.startsWith('sent ')) {
+    const packet = data.substring(5)
+    console.log(`[__RPC__][__SENT__] ${packet}`)
+  }
+  else if (data.startsWith('received ')) {
+    const packet = data.substring(13)
+    console.log(`[__RPC__][RECEIVED] ${packet}`)
   }
   else if (data === 'hello') console.log(`[SNIFFER][INIT] Sniffer connected successfully`)
   else console.log(`[SNIFFER][UNKNOWN] ${data}`)
